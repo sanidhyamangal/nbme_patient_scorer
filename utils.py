3f784b9c73bd167a3db9b3b2a289ff02bf4cb2f3
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2022-03-29 11:34:48 -0500

    added: missing file

---
+++ utils.py
@@ -0,0 +1,19 @@
+"""
+author:Sanidhya Mangal
+github:sanidhyamangal
+"""
+
+
+def get_model_performance(y_true, y_pred):
+    model_performance = y_true - y_pred
+
+    total = model_performance.shape[0]
+
+    neg_count = 0
+
+    for i in model_performance:
+        if sum(i) != 0:
+            neg_count +=1
+    
+
+    return (1- (neg_count/total))
